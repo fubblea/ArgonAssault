504da39225a5235b25d3e7f2a25fe8376cb73185
fubblea <ajay.arthur@gmail.com>
2020-03-14 15:52:59 +0400

    Added Particles and Better Movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,29 +5,57 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
+	float xThrow;
 	[Tooltip("In ms^-1")] [SerializeField] float xSpeed = 40f;
 	[SerializeField] float xRange = 9f;
 
+	float yThrow;
 	[Tooltip("In ms^-1")] [SerializeField] float ySpeed = 40f;
 	[SerializeField] float yRange = 5f;
+
+	[SerializeField] float positionPitchFactor = -1f;
+	[SerializeField] float throwPitchFactor = -25f;
+
+	[SerializeField] float postionYawFactor = 2.2f;
+
+	[SerializeField] float throwRollFactor = -25f;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+
+	private void ProcessTranslation()
+	{
+		xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
 		float xOffset = xThrow * xSpeed * Time.deltaTime;
 		float rawNewXPos = transform.localPosition.x + xOffset;
 		float NewXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
-		
-		float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+		yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 		float yOffset = yThrow * ySpeed * Time.deltaTime;
 		float rawNewYPos = transform.localPosition.y + yOffset;
 		float NewYPos = Mathf.Clamp(rawNewYPos, -yRange, yRange);
 
 		transform.localPosition = new Vector3(NewXPos, NewYPos, transform.localPosition.z);
 	}
+
+	private void ProcessRotation()
+	{
+		float pitch = transform.localPosition.y * positionPitchFactor + yThrow * throwPitchFactor;
+		float yaw = transform.localPosition.x * postionYawFactor;
+		float roll = xThrow * throwRollFactor;
+
+		transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		ProcessTranslation();
+		ProcessRotation();
+
+	}
+
+	
+
 }
