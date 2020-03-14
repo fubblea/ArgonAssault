7bc7f009c37d8d7ae56afed45f58c8095fa47cb9
fubblea <ajay.arthur@gmail.com>
2020-03-14 14:45:37 +0400

    Initial Commit

---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	[Tooltip("In ms^-1")] [SerializeField] float xSpeed = 40f;
+	[SerializeField] float xRange = 9f;
+
+	[Tooltip("In ms^-1")] [SerializeField] float ySpeed = 40f;
+	[SerializeField] float yRange = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+		float xOffset = xThrow * xSpeed * Time.deltaTime;
+		float rawNewXPos = transform.localPosition.x + xOffset;
+		float NewXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+		
+		float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+		float yOffset = yThrow * ySpeed * Time.deltaTime;
+		float rawNewYPos = transform.localPosition.y + yOffset;
+		float NewYPos = Mathf.Clamp(rawNewYPos, -yRange, yRange);
+
+		transform.localPosition = new Vector3(NewXPos, NewYPos, transform.localPosition.z);
+	}
+}
---
+++ Assets/Scripts/splashScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class splashScreen : MonoBehaviour {
+
+	void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+	void Start () {
+		Invoke("loadNextScene", 5);
+	}
+
+	void loadNextScene()
+	{
+		EditorSceneManager.LoadScene(EditorSceneManager.GetActiveScene().buildIndex + 1);
+	}
+	
+}
